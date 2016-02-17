db187082ee92ed2abb6b56866fdb7b325290397e
hongt <2357831@qq.com>
2016-02-17 17:34:06 +0800

    0.9.7.7 坊主登录后页面

---
+++ js/project.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hh on 2016/2/17.
+ */
+$(function () {
+
+    $(".shaixuan li").click(function () {
+        $(this).siblings('li').removeClass("on");
+        $(this).addClass("on")
+
+
+    });
+    $(".cjbtn").click(function () {
+        $(this).siblings().removeClass("cjbtn-on");
+        $(this).addClass("cjbtn-on")
+
+
+    });
+});
